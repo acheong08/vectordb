9c6d2a985a78e4e37f27b68f219a45d65adbf94f
Antonio <acheong@student.dalat.org>
2023-05-01 00:04:22 +0800

    Write basic test

---
+++ semantic_search_test.go
@@ -0,0 +1,17 @@
+package semantic_search_go_test
+
+import (
+	"testing"
+
+	search "github.com/acheong08/semantic-search-go"
+)
+
+func TestSemanticSearch(t *testing.T) {
+	results, err := search.SemanticSearch([]string{"I need a web browser"}, []string{"Google Chrome", "Firefox", "Dumpster Fire", "Garbage", "Brave"}, 2)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	if results[0].CorpusID != 0 {
+		t.Errorf("Expected 0, got %d", results[0].CorpusID)
+	}
+}
