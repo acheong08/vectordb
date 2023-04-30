4e87724bea4e239d6620503f4092f88f66463de9
Antonio <acheong@student.dalat.org>
2023-05-01 00:19:46 +0800

    add examples

---
+++ examples/simple/simple.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	semantic_search "github.com/acheong08/semantic-search-go"
+)
+
+func main() {
+	corpus := []string{"Google Chrome", "Firefox", "Dumpster Fire", "Garbage", "Brave"}
+	results, _ := semantic_search.SemanticSearch([]string{"I need a web browser"}, corpus, 2)
+	for _, result := range results[0] {
+		println(corpus[result.CorpusID])
+		println(result.Score)
+	}
+}
