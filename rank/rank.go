6d3f9198629c7a7a4df9b491b6df1791b095b774
Antonio <acheong@student.dalat.org>
2023-05-03 11:41:17 +0800

    go routines

--- rank/rank.go
+++ rank/rank.go
@@ -15,55 +15,68 @@ func dotProduct(a, b []float64) float64 {
 	return result
 }
 
-func cosSim(queryEmbeddings, corpusEmbeddings typings.Tensor) typings.Tensor {
+func cosSim(queryEmbeddings, corpusEmbeddings typings.Tensor, queryStartIdx, corpusStartIdx int, topK int, resultChan chan<- [][]typings.SearchResult) {
 	numQueries := len(queryEmbeddings)
 	numCorpus := len(corpusEmbeddings)
-	cosScores := make(typings.Tensor, numQueries)
+	queriesResultList := make([][]typings.SearchResult, numQueries)
 
-	for i := 0; i < numQueries; i++ {
-		cosScores[i] = make([]float64, numCorpus)
+	for queryItr := 0; queryItr < numQueries; queryItr++ {
+		scores := make([]float64, numCorpus)
 		for j := 0; j < numCorpus; j++ {
-			cosScores[i][j] = dotProduct(queryEmbeddings[i], corpusEmbeddings[j])
+			scores[j] = dotProduct(queryEmbeddings[queryItr], corpusEmbeddings[j])
+		}
+
+		pq := &typings.SearchResultHeap{}
+		heap.Init(pq)
+
+		for i, score := range scores {
+			if pq.Len() < topK {
+				heap.Push(pq, typings.SearchResult{CorpusID: i, Score: score})
+			} else if score > pq.Peek().Score {
+				heap.Pop(pq)
+				heap.Push(pq, typings.SearchResult{CorpusID: i, Score: score})
+			}
+
+			// Break the loop when topK results have been processed
+			if i == topK-1 {
+				break
+			}
+		}
+
+		for pq.Len() > 0 {
+			result := heap.Pop(pq).(typings.SearchResult)
+			result.CorpusID += corpusStartIdx
+			queriesResultList[queryItr] = append(queriesResultList[queryItr], result)
+
 		}
 	}
 
-	return cosScores
+	resultChan <- queriesResultList
 }
 
 func Rank(queryEmbeddings, corpusEmbeddings typings.Tensor, topK int) [][]typings.SearchResult {
 	const queryChunkSize, corpusChunkSize = 100, 1000
 	queriesResultList := make([][]typings.SearchResult, len(queryEmbeddings))
+	resultChan := make(chan [][]typings.SearchResult)
 
 	for queryStartIdx := 0; queryStartIdx < len(queryEmbeddings); queryStartIdx += queryChunkSize {
 		for corpusStartIdx := 0; corpusStartIdx < len(corpusEmbeddings); corpusStartIdx += corpusChunkSize {
 			queryEndIdx := min(queryStartIdx+queryChunkSize, len(queryEmbeddings))
 			corpusEndIdx := min(corpusStartIdx+corpusChunkSize, len(corpusEmbeddings))
 
-			cosScores := cosSim(queryEmbeddings[queryStartIdx:queryEndIdx], corpusEmbeddings[corpusStartIdx:corpusEndIdx])
+			go cosSim(queryEmbeddings[queryStartIdx:queryEndIdx], corpusEmbeddings[corpusStartIdx:corpusEndIdx], queryStartIdx, corpusStartIdx, topK, resultChan)
+		}
+	}
 
-			for queryItr, scores := range cosScores {
-				pq := &typings.SearchResultHeap{}
-				heap.Init(pq)
-
-				for i, score := range scores {
-					if pq.Len() < topK {
-						heap.Push(pq, typings.SearchResult{CorpusID: i, Score: score})
-					} else if score > pq.Peek().Score {
-						heap.Pop(pq)
-						heap.Push(pq, typings.SearchResult{CorpusID: i, Score: score})
-					}
-
-					// Break the loop when topK results have been processed
-					if i == topK-1 {
-						break
-					}
-				}
-
-				queryID := queryStartIdx + queryItr
-				for pq.Len() > 0 {
-					result := heap.Pop(pq).(typings.SearchResult)
-					result.CorpusID += corpusStartIdx
-					queriesResultList[queryID] = append(queriesResultList[queryID], result)
+	for i := 0; i < len(queryEmbeddings)/queryChunkSize+1; i++ {
+		queriesResultChunk := <-resultChan
+		for j := range queriesResultChunk {
+			index := j + i*queryChunkSize
+			if index < len(queriesResultList) {
+				if queriesResultList[index] == nil {
+					queriesResultList[index] = queriesResultChunk[j]
+				} else {
+					queriesResultList[index] = append(queriesResultList[index], queriesResultChunk[j]...)
 				}
 			}
 		}
