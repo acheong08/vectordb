e6b08ffe08e011aecba32de2b60ba215e867761a
Antonio <acheong@student.dalat.org>
2023-05-12 15:50:35 +0800

    write tests

---
+++ database/database_test.go
@@ -0,0 +1,45 @@
+package database_test
+
+import (
+	"math/rand"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/acheong08/semantic-search-go/database"
+)
+
+func TestStoreAndLoad(t *testing.T) {
+	// Create a temporary file for testing
+	tmpFile, err := os.CreateTemp("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name()) // clean up after test
+
+	// Generate some random embeddings for testing
+	embeddings := make([][]float64, 5)
+	for i := range embeddings {
+		embeddings[i] = make([]float64, 5)
+		for j := range embeddings[i] {
+			embeddings[i][j] = rand.Float64()
+		}
+	}
+
+	// Test the Store function
+	err = database.Store(tmpFile.Name(), embeddings)
+	if err != nil {
+		t.Fatalf("Store function returned error: %v", err)
+	}
+
+	// Test the Load function
+	loadedEmbeddings, err := database.Load(tmpFile.Name())
+	if err != nil {
+		t.Fatalf("Load function returned error: %v", err)
+	}
+
+	// Check if the loaded embeddings are the same as the original
+	if !reflect.DeepEqual(embeddings, loadedEmbeddings) {
+		t.Errorf("Loaded embeddings are not the same as original, got: %v, want: %v.", loadedEmbeddings, embeddings)
+	}
+}
