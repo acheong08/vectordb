5e88dacf103f16524ce38a55e66ae0eef25641bd
Antonio <acheong@student.dalat.org>
2023-05-22 11:27:51 +0800

    fix query

--- experimental/client.go
+++ experimental/client.go
@@ -1,6 +1,8 @@
 package experimental
 
 import (
+	"sort"
+
 	"github.com/acheong08/vectordb/rank"
 	"github.com/acheong08/vectordb/typings"
 	"github.com/acheong08/vectordb/vectors"
@@ -32,12 +34,59 @@ func (c *Client) Add(text string) error {
 	return nil
 }
 
-func (c *Client) Query(queries []string, topK int, sorted bool) ([][]typings.SearchResult, error) {
-	embeddings, err := vectors.EncodeMulti(queries)
+func (c *Client) Query(queries string, topK int) ([]typings.SearchResult, []*Document, error) {
+	embeddings, err := vectors.Encode(queries)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return rank.Rank(embeddings, c.DB.GetEmbeddings(), topK, sorted), nil
+
+	var pending_units []*Chunk
+	var true_documents []*Document
+
+	for _, result := range rank.Rank([][]float64{embeddings}, c.DB.GetEmbeddings(), topK, true)[0] {
+		if c.DB.GetUnitByIndex(result.CorpusID).GetType() == "chunk" {
+			pending_units = append(pending_units, c.DB.GetUnitByIndex(result.CorpusID).(*Chunk))
+		} else {
+			true_documents = append(true_documents, c.DB.GetUnitByIndex(result.CorpusID).(*Document))
+		}
+	}
+
+	for len(pending_units) > 0 {
+		no_more_chunks := true
+		next_pending_units := []*Chunk{}
+		for _, chunk := range pending_units {
+			for _, result := range rank.Rank([][]float64{embeddings}, chunk.GetDocumentEmbeddings(), topK, true)[0] {
+				if c.DB.GetUnitByIndex(result.CorpusID).GetType() == "chunk" {
+					chunk := c.DB.GetUnitByIndex(result.CorpusID).(*Chunk)
+					next_pending_units = append(next_pending_units, chunk)
+					no_more_chunks = false
+				} else {
+					true_documents = append(true_documents, c.DB.GetUnitByIndex(result.CorpusID).(*Document))
+				}
+			}
+		}
+		if no_more_chunks {
+			break
+		}
+		pending_units = next_pending_units
+	}
+
+	true_embeddings := make([][]float64, len(true_documents))
+	for i, doc := range true_documents {
+		true_embeddings[i] = doc.GetEmbeddings()
+	}
+
+	ranked_results := rank.Rank([][]float64{embeddings}, true_embeddings, topK, true)[0]
+
+	var searchResults []typings.SearchResult
+	searchResults = append(searchResults, ranked_results...)
+
+	// Sort the search results
+	sort.Slice(searchResults, func(i, j int) bool {
+		return searchResults[i].Score > searchResults[j].Score
+	})
+
+	return searchResults, true_documents, nil
 }
 
 func (c *Client) RemoveByIndex(i int) error {
