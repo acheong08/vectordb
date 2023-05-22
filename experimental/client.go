b13c2acd4f599defedbae71739fae2502acc87f1
Antonio <acheong@student.dalat.org>
2023-05-22 11:37:22 +0800

    Restore multi-query

--- experimental/client.go
+++ experimental/client.go
@@ -31,57 +31,64 @@ func (c *Client) Add(text string) error {
 	})
 	return nil
 }
-
-func (c *Client) Query(queries string, topK int) ([]typings.SearchResult, []*Document, error) {
-	embeddings, err := vectors.Encode(queries)
+func (c *Client) Query(queries []string, topK int) ([][]typings.SearchResult, [][]*Document, error) {
+	embeddings, err := vectors.EncodeMulti(queries)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	pending_units := make([]*Chunk, 0, topK)
-	true_documents := make([]*Document, 0, topK)
+	allSearchResults := make([][]typings.SearchResult, len(queries))
+	allDocuments := make([][]*Document, len(queries))
 
-	for _, result := range rank.Rank([][]float64{embeddings}, c.DB.GetEmbeddings(), topK, true)[0] {
-		unit := c.DB.GetUnitByIndex(result.CorpusID)
-		if unit.GetType() == "chunk" {
-			pending_units = append(pending_units, unit.(*Chunk))
-		} else {
-			true_documents = append(true_documents, unit.(*Document))
-		}
-	}
+	for qIndex, queryEmbedding := range embeddings {
+		pending_units := make([]*Chunk, 0, topK)
+		true_documents := make([]*Document, 0, topK)
 
-	for len(pending_units) > 0 {
-		next_pending_units := make([]*Chunk, 0, len(pending_units))
-		no_more_chunks := true
-		for _, chunk := range pending_units {
-			for _, result := range rank.Rank([][]float64{embeddings}, chunk.GetDocumentEmbeddings(), topK, true)[0] {
-				unit := c.DB.GetUnitByIndex(result.CorpusID)
-				if unit.GetType() == "chunk" {
-					next_pending_units = append(next_pending_units, unit.(*Chunk))
-					no_more_chunks = false
-				} else {
-					true_documents = append(true_documents, unit.(*Document))
-				}
+		for _, result := range rank.Rank([][]float64{queryEmbedding}, c.DB.GetEmbeddings(), topK, true)[0] {
+			unit := c.DB.GetUnitByIndex(result.CorpusID)
+			if unit.GetType() == "chunk" {
+				pending_units = append(pending_units, unit.(*Chunk))
+			} else {
+				true_documents = append(true_documents, unit.(*Document))
 			}
 		}
-		if no_more_chunks {
-			break
+
+		for len(pending_units) > 0 {
+			next_pending_units := make([]*Chunk, 0, len(pending_units))
+			no_more_chunks := true
+			for _, chunk := range pending_units {
+				for _, result := range rank.Rank([][]float64{queryEmbedding}, chunk.GetDocumentEmbeddings(), topK, true)[0] {
+					unit := c.DB.GetUnitByIndex(result.CorpusID)
+					if unit.GetType() == "chunk" {
+						next_pending_units = append(next_pending_units, unit.(*Chunk))
+						no_more_chunks = false
+					} else {
+						true_documents = append(true_documents, unit.(*Document))
+					}
+				}
+			}
+			if no_more_chunks {
+				break
+			}
+			pending_units = next_pending_units
 		}
-		pending_units = next_pending_units
+
+		true_embeddings := make([][]float64, len(true_documents))
+		for i, doc := range true_documents {
+			true_embeddings[i] = doc.GetEmbeddings()
+		}
+
+		ranked_results := rank.Rank([][]float64{queryEmbedding}, true_embeddings, topK, true)[0]
+
+		searchResults := make([]typings.SearchResult, 0, len(ranked_results))
+		searchResults = append(searchResults, ranked_results...)
+
+		allSearchResults[qIndex] = searchResults
+		allDocuments[qIndex] = true_documents
 	}
 
-	true_embeddings := make([][]float64, len(true_documents))
-	for i, doc := range true_documents {
-		true_embeddings[i] = doc.GetEmbeddings()
-	}
-
-	ranked_results := rank.Rank([][]float64{embeddings}, true_embeddings, topK, true)[0]
-
-	searchResults := make([]typings.SearchResult, 0, len(ranked_results))
-	searchResults = append(searchResults, ranked_results...)
-	return searchResults, true_documents, nil
+	return allSearchResults, allDocuments, nil
 }
-
 func (c *Client) RemoveByIndex(i int) error {
 	c.DB.RemoveByIndex(i)
 	return nil
