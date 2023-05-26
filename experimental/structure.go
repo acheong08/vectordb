35505fbb175e5d11755e4e19bf4ceaea9545cbbb
Antonio <acheong@student.dalat.org>
2023-05-26 10:03:52 +0800

    Client

--- experimental/structure.go
+++ experimental/structure.go
@@ -32,3 +32,15 @@ type ChunkInterface interface {
 	// Interface returned should be of type Documents or Chunk
 	Query(query string, topK int, sorted bool) (*interface{}, error)
 }
+type Client struct {
+	Chunk ChunkInterface
+}
+
+type ClientInterface interface {
+	// This should handle encoding and finding the correct chunk to add the document to
+	Add(text string) error
+	// Queries return a pointer to a Documents struct. This means that you can remove documents from the returned struct and it will be reflected in the database.
+	Query(queries []string, topK int, sorted bool) (*Documents, error)
+	// Returns a Chunk in an optimized format. It might be difficult to write to the returned database
+	GetOptimized() (Chunk, error)
+}
